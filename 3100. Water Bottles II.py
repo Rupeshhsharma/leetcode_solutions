6567153b615e3ea4216c4f36cb0b21bd5a2500da
Rupesh <206439536+Rupeshhsharma@users.noreply.github.com>
2025-10-03 23:15:47 +0530

    Update 3100. Water Bottles II.py

--- 3100. Water Bottles II.py
+++ 3100. Water Bottles II.py
@@ -3,7 +3,8 @@ You are given two integers numBottles and numExchange.numBottles represents the 
 Drink any number of full water bottles turning them into empty bottles.
 Exchange numExchange empty bottles with one full water bottle. Then, increase numExchange by one.
 Note that you cannot exchange multiple batches of empty bottles for the same value of numExchange. For example, if numBottles == 3 and numExchange == 1, you cannot exchange 3 empty water bottles for 3 full bottles.
-Return the maximum number of water bottles you can drink.
+Return the maximum number of water bottles you can drink.                                                                                                                                                                                                  
+                                                                                                                                                                                                  
 Example 1:
 Input: numBottles = 13, numExchange = 6
 Output: 15
