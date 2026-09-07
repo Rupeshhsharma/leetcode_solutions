dd82d663d6626402d214cd66dd29a02895726958
Rupesh <206439536+Rupeshhsharma@users.noreply.github.com>
2026-09-07 11:39:54 +0530

    Add solution for Valid Parentheses problem
    Implement a solution to check for valid parentheses using a stack.

---
+++ 20. Valid Parentheses.py
@@ -0,0 +1,26 @@
+link-https://leetcode.com/problems/valid-parentheses/description/
+code:
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack=[]
+        dict={
+            '}':'{',
+            ')':'(',
+            ']':'[',
+        }
+        for i in s:
+            if i not in dict:
+                stack.append(i)  
+            else:
+                if not stack:
+                    return False
+                top=stack[-1]
+                if dict[i]==top:
+                    stack.pop()
+                if dict[i]!=top:
+                    break 
+        if not stack:
+            return True
+        else:
+            return False            
+
